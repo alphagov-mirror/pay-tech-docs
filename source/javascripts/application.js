fd88146287115d3453a606472cb6a958cb014111
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-10-20 16:55:58 +0100

    PP-6284: add tech docs gem analytics init into javascripts
    If we use the gem analytics ID, google analytics .js will always be
    fetched from Google no matter if consent is provided. By forking this
    and moving into our client side code, we can only run it if consent has
    been provided through the common cookie code.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,14 @@
 //= require govuk_tech_docs
+//= require analytics
 //= require_directory ../../node_modules/@govuk-pay/pay-js-commons/lib/analytics/dist/
 
-if (window.GovUkPay) {
-  window.GovUkPay.cookies.showBannerIfConsentNotSet()
-}
+window.addEventListener('DOMContentLoaded', function() {
+  if (window.GovUkPay) {
+    window.GovUkPay.cookies.showBannerIfConsentNotSet(window.initialiseGoogleAnalytics)
+
+    if (window.GovUkPay.cookies.hasAnalyticsConsent()) {
+      window.initialiseGoogleAnalytics()
+    }
+  }
+})
+
